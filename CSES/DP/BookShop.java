3208f0108410f55d9d996c4d531a6976c14b88fb
Tristan Lee <tlee10@stuy.edu>
2021-02-03 15:58:53 -0500

    CSES BookShop

---
+++ CSES/DP/BookShop.java
@@ -0,0 +1,100 @@
+import java.io.*;
+import java.util.*;
+
+public class BookShop {
+    /*
+     *************************INPUT*************************
+     */
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        int[] rlint(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+
+        long[] rllong(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = nextInt();
+            }
+            return a;
+        }
+    }
+
+    static FastScanner input = new FastScanner();
+
+    /*
+     *************************OUTPUT*************************
+     */
+
+    public static void plist(String[] a) {
+        for (String i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void plint(int[] a) {
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void plong(long[] a) {
+        for (long i : a) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void YN(boolean yn) {
+        if (yn) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    /*
+     ************************* PUT CODE HERE *************************
+     */
+    public static void main(String[] args) {
+        int n = input.nextInt();
+        int x = input.nextInt();
+        int[] h = input.rlint(n);
+        int[] s = input.rlint(n);
+        int[][] dp = new int[n+1][x+1];
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 0; j < x+1; j++) {
+                dp[i][j]=dp[i-1][j];
+                if (j-h[i-1]>=0){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-h[i-1]]+s[i-1]);
+                }
+            }
+        }
+        System.out.println(dp[n][x]);
+    }
+}
